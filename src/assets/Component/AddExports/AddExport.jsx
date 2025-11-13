eb1f0a3ea3cce8fef646a95e40e29b43b2c9028a
shahriarahmeddipu <shahriarahmeddipu@shahriarahmeddipus-Mac-mini.local>
2025-11-12 19:01:22 -0500

    Implemented same card design and call data to MyExport File

--- src/assets/Component/AddExports/AddExport.jsx
+++ src/assets/Component/AddExports/AddExport.jsx
@@ -29,7 +29,7 @@ const { user } = use(AuthContext);
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Convert numeric values properly
+ 
    const productData = {
     ...formData,
     email: user?.email, // 👈 add logged-in user's email here
